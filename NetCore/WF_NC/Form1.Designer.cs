1afc3423fc6af0e0e71009116f7455f635c2ba15
Shyam Gupta <shgu@microsoft.com>
2022-04-06 14:50:46 -0700

    Adding .NET Core project

--- NetCore/WF_NC/Form1.Designer.cs
+++ NetCore/WF_NC/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace NetFxSdk
+﻿namespace WF_NC
 {
     partial class Form1
     {
@@ -28,20 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(243, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(315, 221);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.checkedListBox1);
-            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button button1;
     }
 }
---
+++ NetCore/WF_NC/Form1.cs
@@ -0,0 +1,10 @@
+namespace WF_NC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
